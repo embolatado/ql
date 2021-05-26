c68e8d49bd27fdec955efcafbff01915121a1d9c
embolatado <embolatado@gmail.com>
2021-05-26 12:33:46 -0500

    Template dynamic title default working too :-)

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import inicio
+from .views import about, inicio
 
 urlpatterns = [
     path('', inicio, name="blog-index"),
+    path('about/', about, name="blog-about"),
 ]
 
