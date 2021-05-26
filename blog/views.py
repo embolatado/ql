7ca0590d4800a737fd8cdae82ca2777556e7609f
embolatado <embolatado@gmail.com>
2021-05-26 12:07:06 -0500

    Dynamic title in context

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def inicio(request):
-    return render(request, 'blog/index.html')
+    titulo = "Título dinámico"
+    return render(request, 'blog/index.html', {"titulo": titulo})
 
 
 def about(request):
