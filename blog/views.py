1aa187846891e4da6a550157048ea63d177d0dc4
embolatado <embolatado@gmail.com>
2021-05-25 22:43:21 -0500

    First view and Templates folder created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'blog/index.html')
+
+
+def about(request):
+    return render(request, 'blog/about.html')
